5163d663f7faa1d65698df45a54006d10a3b64a5
Peter Maquiran <peter.maquiran@equilibrium.local>
2023-10-15 19:07:03 +0100

    add service callbackScheduler

---
+++ src/Utility/scheduler/callbackScheduler.ts
@@ -0,0 +1,116 @@
+/**
+ * Manages a queue of callbacks and controls their execution with start, pause, and resume functionality.
+ */
+export class CallbackScheduler {
+  private callbackQueue: (() => void)[] = []; // A queue to store the callbacks.
+  private running: boolean = false; // Tracks whether the scheduler is running.
+
+  /**
+   * Enqueues a callback function to be executed in the queue.
+   * @param callback - The callback function to be executed.
+   * @returns A function to rerun the callback
+   */
+  enqueueCallback(callback: () => void): Function {
+    const add = () => { this.callbackQueue.push(callback); };
+    add();
+    return add;
+  }
+
+  /**
+   * Adds a callback function to the queue without immediately executing it.
+   * @param callback - The callback function to be added to the queue.
+   * @returns A function to remove the callback from the queue.
+   */
+  triggerToEnqueueCallback(callback: () => void): Function {
+    const add = () => { this.callbackQueue.push(callback); };
+    return add;
+  }
+
+  /**
+   * Starts the execution of callbacks in the queue.
+   */
+  start(): void {
+    if (!this.running) {
+      this.running = true;
+      this.executeNextCallback();
+    }
+  }
+
+  /**
+   * Pauses the execution of callbacks.
+   */
+  pause(): void {
+    this.running = false;
+  }
+
+  /**
+   * Resumes the execution of callbacks.
+   */
+  resume(): void {
+    if (!this.running) {
+      this.running = true;
+      this.executeNextCallback();
+    }
+  }
+
+  /**
+   * Executes the next callback in the queue, removing it after execution.
+   */
+  executeNextCallback(): void {
+    if (this.running && this.callbackQueue.length > 0) {
+      const callback = this.callbackQueue.shift();
+      callback();
+      this.executeNextCallback(); // Execute the next callback
+    } else {
+      this.running = false;
+    }
+  }
+}
+
+
+// // Example usage:
+// const manager = new CallbackScheduler();
+
+// manager.enqueueCallback(() => {
+//   console.log("Callback 1 executed.");
+// });
+
+// manager.enqueueCallback(() => {
+//   console.log("Callback 2 executed.");
+// });
+
+// manager.enqueueCallback(() => {
+//   console.log("Callback 3 executed.");
+// });
+
+// manager.start();
+
+// setTimeout(() => {
+//   manager.pause();
+//   console.log("Manager paused.");
+// }, 3000);
+
+// setTimeout(() => {
+//   manager.resume();
+//   console.log("Manager resumed.");
+// }, 6000);
+
+
+// const timingManager = new CallbackScheduler();
+
+// class someResponsibility{
+
+
+//   constructor() {
+
+//     setTimeout(()=> {
+//       timingManager.start()
+//     }, 1000)
+
+//     this.execute()
+//   }
+
+//   execute = timingManager.triggerToEnqueueCallback(()=> {
+
+//   })
+// }
